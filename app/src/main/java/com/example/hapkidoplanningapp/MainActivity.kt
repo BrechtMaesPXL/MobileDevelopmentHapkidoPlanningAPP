552da0b755344a1234b53b925644e7bd7c0db6be
BrechtMaesPXL <brecht.maes@student.pxl.be>
2024-04-02 17:11:43 +0200

    testing to add nav bar

--- app/src/main/java/com/example/hapkidoplanningapp/MainActivity.kt
+++ app/src/main/java/com/example/hapkidoplanningapp/MainActivity.kt
@@ -1,46 +1,42 @@
 package com.example.hapkidoplanningapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hapkidoplanningapp.ui.theme.HapkidoPlanningAppTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HapkidoPlanningAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+        loadFragment(ActivatieFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    loadFragment(ActivatieFragment())
+                    true
                 }
+                R.id.message -> {
+                    loadFragment(LocationFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(UserFragment())
+                    true
+                }
+                else -> false // Added else branch to make 'when' expression exhaustive
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HapkidoPlanningAppTheme {
-        Greeting("Android")
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
+
